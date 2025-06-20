e83f05d455af23ecb5393e0a3fd1f15b0f96c68a
dggilson <devinggilson@gmail.com>
2025-06-20 17:23:45 -0400

    80. Remove Duplicates from sorted array: in-place optimal sorting

--- python/remove_duplicates_from_sorted_array_II.py
+++ python/remove_duplicates_from_sorted_array_II.py
@@ -1,28 +1,15 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
-        free_space = []
-        frequency_hshmp = {}
-        k = 0
-        for i, num in enumerate(nums):
-            #if number has been added and it's only been seen once: update it to 2
-            if num in frequency_hshmp:
-                num_count = frequency_hshmp.get(num)
-                if num_count != 2:
-                    num_count = 2
-                    frequency_hshmp[num] = num_count
-                    #if free space exists: we place there
-                    if len(free_space) != 0:
-                        nums[free_space.pop(0)] = num
-                        free_space.append(i)
-                    k += 1
-                else:
-                    #if value is at 2: mark this as free space
-                    free_space.append(i)
-            else:
-                #number is added to map, if free space exists, we place it there
-                frequency_hshmp[num] = 1
-                if len(free_space) != 0:
-                    nums[free_space.pop(0)] = num
-                    free_space.append(i)
-                k += 1
-        return k
+     def removeDuplicates(self, nums):
+        write_ptr = 2
+        #early exit since no chance of >3 duplicates
+        if len(nums) <= 2:
+            return 2
+        for i in range(2, len(nums)):
+            #if the current value does not match the one 2 positions down:
+            if nums[i] != nums[write_ptr - 2]:
+                #we write
+                nums[write_ptr] = nums[i]
+                write_ptr += 1
+            #else proceed through
+        return write_ptr
+        #note: i and write_ptr are differnt since we are scanning and placing in-place
